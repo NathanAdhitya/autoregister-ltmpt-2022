0427066ec17b5606fe77ac5b8494255ae1aefd81
NathanAdhitya <me@natha.my.id>
2022-01-15 12:24:08 +0700

    🐛 second attempt on fixing the validator

--- utils/Validators.js
+++ utils/Validators.js
@@ -11,29 +11,29 @@ export function validateEnv(env = process.env) {
     const validations = [
         [
             "NISN harus terdiri dari sepuluh angka.",
-            env["NISN"] && String(env["NISN"]).length === 10,
+            env["NISN"] && env["NISN"].length === 10,
         ],
         [
             "NPSN harus terdiri dari delapan angka.",
-            env["NPSN"] && String(env["NPSN"]).length === 8,
+            env["NPSN"] && env["NPSN"].length === 8,
         ],
         [
             "TGL_LAHIR harus sesuai format DD/MM/YYYY",
             env["TGL_LAHIR"] &&
-                String(env["TGL_LAHIR"]).match(/^\d\d\/\d\d\/\d\d\d\d$/),
+                env["TGL_LAHIR"].match(/^\d\d\/\d\d\/\d\d\d\d$/) != null,
         ],
         [
             "EMAIL tidak valid.",
             env["EMAIL"] &&
-                String(env["EMAIL"]).match(
+                env["EMAIL"].match(
                     // eslint-disable-next-line no-useless-escape
                     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                ),
+                ) !== null,
         ],
         [
             "PASSWORD tidak valid, harus hanya terdiri dari huruf dan angka dan minimal 8 karakter.",
             env["PASSWORD"] &&
-                String(env["PASSWORD"]).match(/^[a-zA-Z0-9]{8,}$/),
+                env["PASSWORD"].match(/^[a-zA-Z0-9]{8,}$/) !== null,
         ],
     ];
 
